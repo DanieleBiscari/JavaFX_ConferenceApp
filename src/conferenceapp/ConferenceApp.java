eeefd7a89f1ba4537adaac4c9fd136ab6e8b1224
alfon <alfon@Alphy>
2025-06-15 16:02:32 +0200

    Inizzializzazione del progetto

--- src/conferenceapp/ConferenceApp.java
+++ src/conferenceapp/ConferenceApp.java
@@ -1,20 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package conferenceapp;
+
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
- * @author V10649
+ * @author alfon
  */
-public class ConferenceApp {
+public class ConferenceApp extends Application{
 
     /**
      * @param args the command line arguments
      */
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML_Login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
