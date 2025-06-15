594998f07e80e3fb7477f10c0511324146d3fce3
Daniele Biscari <Daniele.Biscari@Sistinf.it>
2025-06-15 15:31:12 +0200

    Prima commit, setup di JavaFx e SceneBuilder

---
+++ src/conferenceapp/ConferenceApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conferenceapp;
+
+/**
+ *
+ * @author V10649
+ */
+public class ConferenceApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
